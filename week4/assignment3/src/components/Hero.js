df8eb583d23882d1920abdd284aee81bce7d0841
Candy Ho <candyho.learning@gmail.com>
2024-02-05 21:06:00 +0800

    Added toggle welcome message feature

--- week4/assignment3/src/components/Hero.js
+++ week4/assignment3/src/components/Hero.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Hero() {
+  const [welcome, setWelcome] = useState(true);
+  const handleClick = () => {
+    setWelcome(!welcome);
+  };
   return (
-    <section className="hero">
-      <h1>Welcome Message - Week 2</h1>
+    <section className="hero" onClick={handleClick}>
+      <h1>{welcome ? "Welcome Message" : "Have a Good Time!"}</h1>
     </section>
   );
 }
